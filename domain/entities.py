2c383b4f7d38fd161ebbf825292ebb6826d7678e
vladushka2000 <nazipovvt@yandex.ru>
2024-12-12 13:19:40 +0500

    chore(entities): Добавлен метод в LogFile

--- domain/entities.py
+++ domain/entities.py
@@ -18,12 +18,18 @@ class LogFile(domain.IEntity):
         self.entries: list[value_objects.Entry] = []
         self.stats = value_objects.Stats()
 
+    def add_entry(self, entry: value_objects.Entry) -> None:
+        """
+        Добавить запись
+        :param entry: запись
+        """
+
+        self.entries.append(entry)
+
     def calc_stats(self) -> None:
         """
         Посчитать статистику
         """
-
-        self.stats.tasks_count = len(self.entries)
 
         success_tasks_count = 0
         fail_tasks_count = 0
@@ -32,7 +38,7 @@ class LogFile(domain.IEntity):
         is_task_running = False
 
         # Расчет успешных и неуспешных тасок
-        for entry in self.entries:
+        for entry_num, entry in enumerate(self.entries):
             if entry.msg_type == enums.MessageType.START and not is_task_running:
                 is_task_running = True
                 task_start_time = entry.time
@@ -41,7 +47,12 @@ class LogFile(domain.IEntity):
                 success_tasks_count += 1
                 exec_times.append(entry.time - task_start_time)
             else:
-                is_task_running = False
                 fail_tasks_count += 1
 
-        self.stats.mean_time = sum(exec_times) / len(exec_times)
+            if entry.msg_type == enums.MessageType.START and entry_num == len(self.entries) - 1:
+                fail_tasks_count += 1
+
+        self.stats.tasks_count = success_tasks_count + fail_tasks_count
+        self.stats.success_tasks = success_tasks_count
+        self.stats.fail_tasks = fail_tasks_count
+        self.stats.mean_time = int(sum(exec_times) / len(exec_times))
